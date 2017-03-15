479f9348a1f59b9bcd9a27de140b6ea2812dfae9
pparonson <preston.aronson@gmail.com>
2017-03-15 09:16:22 -0600

    edit clock component

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,31 +1,39 @@
 const React = require('react');
 
 const Clock = React.createClass({
+  // specify default props if not passed down from parent
   getDefaultProps() {
     totalSeconds: 0
   },
+  // define propTypes
   propTypes: {
-    // Specify propTypes for totalSeconds
     totalSeconds: React.PropTypes.number
   },
+
   formatSeconds(totalSeconds) {
-    let seconds = totalSeconds % 60;
+    // separate totalSeconds into proper min:sec format
+    // let permits reassignment
     let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
 
     if (seconds < 10) {
+      // prepend a 0 to sec
       seconds = `0${seconds}`;
     }
 
     if (minutes < 10) {
+      // prepend a 0 to min
       minutes = `0${minutes}`;
     }
 
     return `${minutes}:${seconds}`;
   },
+
   render() {
+    // get props using destructuring
     const {totalSeconds} = this.props;
     return (
-      <div className="clock">
+      <div className="clock" ref="clock">
         <span className="clock-text">
           {this.formatSeconds(totalSeconds)}
         </span>
--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,3 +1,4 @@
+// a standard set of react modules for proper BDD testing
 const React = require('react');
 const ReactDOM = require('react-dom');
 const expect = require('expect');
@@ -16,8 +17,9 @@ describe('render', () => {
   it('should render clock to output', () => {
     // get clock component and pass totalSeconds as prop
     const clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
-    // use jQuery selector to get element and conv to jQuery obj
+    // use jQuery selector to get element and conv to jQuery HTML obj
     const $el = $(ReactDOM.findDOMNode(clock));
+    // get text value from .clock-text element (ie 01:02)
     const actualText = $el.find('.clock-text').text();
 
     expect(actualText).toBe('01:02');
@@ -26,7 +28,7 @@ describe('render', () => {
 
 describe('formatSeconds', () => {
   it('should format seconds', () => {
-    // TestUtils renders and returns component
+    // TestUtils renders and returns component for test use
     const clock = TestUtils.renderIntoDocument(<Clock/>);
     const seconds = 615;
     const expected = '10:15';
@@ -34,6 +36,7 @@ describe('formatSeconds', () => {
     const actual = clock.formatSeconds(seconds);
     expect(actual).toBe(expected);
   });
+
   it('should format seconds when min/sec < 10', () => {
     const clock = TestUtils.renderIntoDocument(<Clock/>);
     const seconds = 61;
