3645b4c6d734775f9a22098a323121c6eaca1bff
pparonson <preston.aronson@gmail.com>
2017-03-15 07:35:10 -0600

    edit routing configurations and documentation

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,7 @@ const ReactDOM = require('react-dom');
 // react router library used to configure url routes for web app
 const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+// loading components
 const Main = require('Main');
 const Timer = require('Timer');
 const Countdown = require('Countdown');
@@ -12,10 +13,12 @@ $(document).foundation();
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 require('style!css!sass!applicationStyles')
 
+// component routing
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="countdown" component={Countdown}></Route>
+      {/* default route IndexRoute, renders if 0 route paths */}
       <IndexRoute component={Timer}></IndexRoute>
     </Route>
   </Router>,
